d3f26b3bb3b69433655f0eb71d6a4e2f37aac513
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-01-17 00:14:41 +0700

    Data Widget

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Person;
 
 class HomeController extends Controller
 {
@@ -23,7 +24,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $ret['total_persons'] = Person::all()->count();
+        $ret['total_persons_permanent'] = Person::where("citizens_status_id",1)->count();
+        $ret['total_persons_temporary'] = Person::where("citizens_status_id",2)->count();
+
+        return view('home', $ret);
     }
 
     public function chart()
