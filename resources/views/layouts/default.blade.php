d1f4b79a3b59ac4f64e3272c34f214a7cbc5c1cc
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-01-17 22:57:26 +0700

    ^ Update Copyright

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -49,11 +49,11 @@
                 <!-- Footer START -->
                 <footer class="footer">
                     <div class="footer-content">
-                        <p class="m-b-0">Copyright © 2019 Theme_Nate. All rights reserved.</p>
-                        <span>
+                        <p class="m-b-0">Copyright © 2021 All rights reserved.</p>
+                        {{-- <span>
                             <a href="" class="text-gray m-r-15">Term &amp; Conditions</a>
                             <a href="" class="text-gray">Privacy &amp; Policy</a>
-                        </span>
+                        </span> --}}
                     </div>
                 </footer>
                 <!-- Footer END -->
