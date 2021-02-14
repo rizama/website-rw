345b1920e3da9487ac857d76a504d57398177af4
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-02-14 12:44:46 +0700

    ^add table to dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,8 @@
 @endsection
 
 @section('css')
+<link href="{{ url('templates/enlinkadmin-10/demo/app') }}/assets/vendors/datatables/dataTables.bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css" rel="stylesheet">
 <style>
 .highcharts-figure, .highcharts-data-table table {
     min-width: 320px; 
@@ -96,6 +98,55 @@ input[type="number"] {
         </div>
     </div>
     <div class="row">
+        <div class="col-md-12 col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h4>Daftar Penduduk</h4>
+                        </div>                        
+                    </div>
+                    <div class="m-t-25">
+                        <div class="table-responsive">
+                            <table id="data-table" class="table">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama</th>
+                                        <th>TTL</th>
+                                        <th>Umur</th>
+                                        <th>RT</th>
+                                        <th>Status Warga</th>
+                                        <th>Pekerjaan</th>
+                                        <th>Jenis Kelamin</th>
+                                        <th>Pendidikan</th>
+                                        <th>Agama</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($all_persons as $key => $person)
+                                    <tr>
+                                        <td>{{$key+1}}</td>
+                                        <td>{{$person->name}}</td>
+                                        <td>{{$person->place_of_birth}}, {{\Carbon\Carbon::parse($person->date_of_birth)->toFormattedDateString()}}</td>
+                                        <td>{{\Carbon\Carbon::parse($person->date_of_birth)->diff(\Carbon\Carbon::now())->format('%y Tahun')}}</td>
+                                        <td>{{$person->rt_number}}</td>
+                                        <td>{{$person->status->name}}</td>
+                                        <td>{{$person->job->name}}</td>
+                                        <td>{{$person->gender  == "L" ? "Laki-laki" : "Perempuan"}}</td>
+                                        <td>{{$person->education->name}}</td>
+                                        <td>{{$person->religion}}</td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
         <div class="col-md-6">
             <div class="card">
                 <div class="card-body">
@@ -154,6 +205,24 @@ input[type="number"] {
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
 <script src="https://code.highcharts.com/modules/export-data.js"></script>
 <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+<script src="{{ url('templates/enlinkadmin-10/demo/app') }}/assets/vendors/datatables/jquery.dataTables.min.js"></script>
+<script src="{{ url('templates/enlinkadmin-10/demo/app') }}/assets/vendors/datatables/dataTables.bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
+
+<script>
+    $('#data-table').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+        'copy', 'csv', 'excel', 'pdf', 'print'
+    ]
+});
+</script>
+
 <script>
     let chartColors = {
         red: 'rgb(255, 99, 132)',
